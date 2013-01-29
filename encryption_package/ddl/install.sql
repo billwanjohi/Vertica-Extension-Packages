e3c466a40e18bcb1ff975da463772416e2673f6d
billwanjohi <admin@Bills-MBP-i7.local>
2013-01-29 16:20:35 -0600

    encryption: grant functions to all users at install

--- encryption_package/ddl/install.sql
+++ encryption_package/ddl/install.sql
@@ -6,4 +6,5 @@ CREATE LIBRARY Encryption as :libfile;
 CREATE FUNCTION AESEncrypt as language 'C++' name 'AESEncryptFactory' library Encryption;
 CREATE FUNCTION AESDecrypt as language 'C++' name 'AESDecryptFactory' library Encryption;
 
-
+GRANT ALL ON FUNCTION public.AESEncrypt (x varchar, y varchar) TO public;
+GRANT ALL ON FUNCTION public.AESDecrypt (x varchar, y varchar) TO public;
