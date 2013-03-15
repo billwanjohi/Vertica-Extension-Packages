5657ff686b35fc767ccc1481558e69fad4cbed48
billwanjohi <kaguru@gmail.com>
2013-03-14 22:38:50 -0500

    replace functions if already exist

--- encryption_package/ddl/install.sql
+++ encryption_package/ddl/install.sql
@@ -3,8 +3,8 @@ select version();
 \set libfile '\''`pwd`'/lib/Encryption.so\'';
 
 CREATE LIBRARY Encryption as :libfile;
-CREATE FUNCTION AESEncrypt as language 'C++' name 'AESEncryptFactory' library Encryption;
-CREATE FUNCTION AESDecrypt as language 'C++' name 'AESDecryptFactory' library Encryption;
+CREATE OR REPLACE FUNCTION AESEncrypt as language 'C++' name 'AESEncryptFactory' library Encryption;
+CREATE OR REPLACE FUNCTION AESDecrypt as language 'C++' name 'AESDecryptFactory' library Encryption;
 
 GRANT ALL ON FUNCTION public.AESEncrypt (x varchar, y varchar) TO public;
 GRANT ALL ON FUNCTION public.AESDecrypt (x varchar, y varchar) TO public;
--- strings_package/ddl/install.sql
+++ strings_package/ddl/install.sql
@@ -3,17 +3,17 @@ select version();
 \set libfile '\''`pwd`'/lib/StringsLib.so\'';
 CREATE LIBRARY StringsLib AS :libfile;
 
-CREATE FUNCTION EditDistance                   AS LANGUAGE 'C++' NAME 'EditDistanceFactory'         LIBRARY StringsLib;
-CREATE FUNCTION Stemmer                        AS LANGUAGE 'C++' NAME 'PorterStemmerFactory'        LIBRARY StringsLib;
-CREATE TRANSFORM FUNCTION StringTokenizer      AS LANGUAGE 'C++' NAME 'StringTokenizerFactory'      LIBRARY StringsLib;
-CREATE TRANSFORM FUNCTION StringTokenizerDelim AS LANGUAGE 'C++' NAME 'StringTokenizerDelimFactory' LIBRARY StringsLib;
-CREATE TRANSFORM FUNCTION TwoGrams             AS LANGUAGE 'C++' NAME 'TwoGramsFactory'             LIBRARY StringsLib;
-CREATE TRANSFORM FUNCTION ThreeGrams           AS LANGUAGE 'C++' NAME 'ThreeGramsFactory'           LIBRARY StringsLib;
-CREATE TRANSFORM FUNCTION FourGrams            AS LANGUAGE 'C++' NAME 'FourGramsFactory'            LIBRARY StringsLib;
-CREATE TRANSFORM FUNCTION FiveGrams            AS LANGUAGE 'C++' NAME 'FiveGramsFactory'            LIBRARY StringsLib;
-CREATE FUNCTION WordCount                      AS LANGUAGE 'C++' NAME 'WordCountFactory'            LIBRARY StringsLib;
-CREATE TRANSFORM FUNCTION gen_anagram          AS LANGUAGE 'C++' NAME 'AnagramFactory'              LIBRARY StringsLib;
-CREATE TRANSFORM FUNCTION group_concat         AS LANGUAGE 'C++' NAME 'GroupConcatFactory'          LIBRARY StringsLib;
+CREATE OR REPLACE FUNCTION EditDistance                   AS LANGUAGE 'C++' NAME 'EditDistanceFactory'         LIBRARY StringsLib;
+CREATE OR REPLACE FUNCTION Stemmer                        AS LANGUAGE 'C++' NAME 'PorterStemmerFactory'        LIBRARY StringsLib;
+CREATE OR REPLACE TRANSFORM FUNCTION StringTokenizer      AS LANGUAGE 'C++' NAME 'StringTokenizerFactory'      LIBRARY StringsLib;
+CREATE OR REPLACE TRANSFORM FUNCTION StringTokenizerDelim AS LANGUAGE 'C++' NAME 'StringTokenizerDelimFactory' LIBRARY StringsLib;
+CREATE OR REPLACE TRANSFORM FUNCTION TwoGrams             AS LANGUAGE 'C++' NAME 'TwoGramsFactory'             LIBRARY StringsLib;
+CREATE OR REPLACE TRANSFORM FUNCTION ThreeGrams           AS LANGUAGE 'C++' NAME 'ThreeGramsFactory'           LIBRARY StringsLib;
+CREATE OR REPLACE TRANSFORM FUNCTION FourGrams            AS LANGUAGE 'C++' NAME 'FourGramsFactory'            LIBRARY StringsLib;
+CREATE OR REPLACE TRANSFORM FUNCTION FiveGrams            AS LANGUAGE 'C++' NAME 'FiveGramsFactory'            LIBRARY StringsLib;
+CREATE OR REPLACE FUNCTION WordCount                      AS LANGUAGE 'C++' NAME 'WordCountFactory'            LIBRARY StringsLib;
+CREATE OR REPLACE TRANSFORM FUNCTION gen_anagram          AS LANGUAGE 'C++' NAME 'AnagramFactory'              LIBRARY StringsLib;
+CREATE OR REPLACE TRANSFORM FUNCTION group_concat         AS LANGUAGE 'C++' NAME 'GroupConcatFactory'          LIBRARY StringsLib;
 
 GRANT ALL ON FUNCTION public.editdistance (x varchar, y varchar) TO public;
 GRANT ALL ON FUNCTION public.wordcount(x varchar) TO public;
